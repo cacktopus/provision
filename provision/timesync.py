b106b92f437c53c3a2a5adf74f5d5621d9c1048c
Jeremy Suurkivi <suurkivi@gmail.com>
2021-01-16 17:25:45 -0800

    provision

--- provision/timesync.py
+++ provision/timesync.py
@@ -13,16 +13,12 @@ class Timesync(Service):
         return self.exe()
 
     def env(self) -> Dict[str, str]:
-        return {"RTC": "1"} if self.has_rtc() else {}
+        return self.ctx.record.env.get('timesync', {}) | {
+            "RTC": str(int(self.has_rtc()))
+        }
 
     def setup(self) -> None:
         self.get_tar_archive()
-
-    def register_service(self) -> None:
-        # TODO: have tags or extra_tags be a function rather than calling this
-        tags = ['rtc'] if self.has_rtc() else []
-        assert self.port is not None
-        self.register_service_with_consul(self.name, self.port, tags=tags)
 
     def has_rtc(self) -> bool:
         return 'rtc' in self.ctx.record.tags
