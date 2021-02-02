6e6139d6c4b2a0c9fc2750b108755034bfe9434e
Jeremy Suurkivi <suurkivi@gmail.com>
2021-02-02 15:04:57 -0800

    rtunneld

--- provision/rtunneld.py
+++ provision/rtunneld.py
@@ -1,22 +1,30 @@
+from typing import Dict
+
 import provision.hashicorp_vault as hashicorp_vault
+
 from .service import Service
 
 
 class Rtunneld(Service):
     name = "rtunneld"
     description = "reverse ssh tunnel manager"
-    deps = ["service-ready"]
-    repo = "rtunneld"
+    deps = ["syncthing"]
+
+    # def extra_groups(self) -> List[str]:
+    #     return super().extra_groups() + ["i2c", "gpio", "audio"]
+    #
+    def env(self) -> Dict[str, str]:
+        return self.ctx.record.env['rtunneld']
 
     def command_line(self) -> str:
         return self.exe()
 
     def setup(self) -> None:
+        self.get_tar_archive()
+
         vault_client = hashicorp_vault.Client()
         secret = vault_client.get("ssh/tunnel")
         rsa: str = secret['id_rsa']
-
-        self.build()
 
         # TODO: move this out of .ssh
         remote_file = self.user_home(".ssh", 'tunnel.id_rsa')
