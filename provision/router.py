397741eb86445976622a0fce0f15ddc293403b1f
Jeremy Suurkivi <suurkivi@gmail.com>
2022-03-06 16:37:59 -0800

    hostapd

--- provision/router.py
+++ provision/router.py
@@ -1,3 +1,5 @@
+from provision import hashicorp_vault
+
 from .service import Provision
 
 
@@ -13,4 +15,22 @@ class Router(Provision):
             "iptables-persistent",
         ]
 
+        vault_client = hashicorp_vault.Client()
+        wifi = vault_client.get(f"wifi/theheads")
+
+        self.template(
+            name="hostapd.conf",
+            location="/etc/hostapd/hostapd.conf",
+            user="root",
+            group="root",
+            vars=dict(
+                interface="wlan1",
+                ssid="theheads2",
+                wpa_passphrase=wifi["wpa_passphrase"],
+            )
+        )
+
         self.runner.run_remote_rpc("install_packages", params=dict(packages=package_list))
+
+        self.runner.run_remote_rpc("systemctl_unmask", params=dict(service_name="hostapd"))
+        self.runner.run_remote_rpc("systemctl_enable", params=dict(service_name="hostapd", now=True))
